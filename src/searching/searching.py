70194d0df44d6bc6ba733b36e6282cb45cb3dd37
Michelle Scott <scottmm374@gmail.com>
2020-09-06 17:23:11 -0400

    all tests pass on binary recursive

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
 
-new_arr = sorted(arr1)
+
 start_index = 0
-end_index = len(new_arr) - 1
+end_index = len(arr1) - 1
 
 
 def binary_search(arr, target, start, end):
-    arr_length = (start + end)
-    mid_index = (arr_length) // 2
+
+    if len(arr) == 0:
+        return -1
+
+    mid_index = (start + end) // 2
     mid_val = arr[mid_index]
 
     if target == mid_val:
@@ -17,21 +20,21 @@ def binary_search(arr, target, start, end):
     if target != mid_val:
 
         if target > mid_val:
-            if start == mid_val:
+            if start == mid_index:
                 return -1
             else:
                 start = mid_index
                 return binary_search(arr, target, start, end)
 
         if target < mid_val:
-            if end == mid_val:
+            if end == mid_index:
                 return -1
             else:
                 end = mid_index
                 return binary_search(arr, target, start, end)
 
 
-x = binary_search(new_arr, -9, start_index, end_index)
+x = binary_search(arr1, -10, start_index, end_index)
 print(x)
 
 

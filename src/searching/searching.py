0c420ee77337b21cfff2629c068d15ba8034c34b
Michelle Scott <scottmm374@gmail.com>
2020-09-06 17:10:07 -0400

    Binary rec working, but not passing test

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,46 @@
 # TO-DO: Implement a recursive implementation of binary search
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+
+new_arr = sorted(arr1)
+start_index = 0
+end_index = len(new_arr) - 1
+
+
 def binary_search(arr, target, start, end):
-    # Your code here
+    arr_length = (start + end)
+    mid_index = (arr_length) // 2
+    mid_val = arr[mid_index]
+
+    if target == mid_val:
+        return mid_index
+
+    if target != mid_val:
+
+        if target > mid_val:
+            if start == mid_val:
+                return -1
+            else:
+                start = mid_index
+                return binary_search(arr, target, start, end)
+
+        if target < mid_val:
+            if end == mid_val:
+                return -1
+            else:
+                end = mid_index
+                return binary_search(arr, target, start, end)
 
 
-# STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+x = binary_search(new_arr, -9, start_index, end_index)
+print(x)
+
+
+# !STRETCH: implement an order-agnostic binary search
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
 
+# def agnostic_binary_search(arr, target):
+#     # Your code here
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,61 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+# def merge(arrA, arrB):
+#     elements = len(arrA) + len(arrB)
+#     merged_arr = [0] * elements
 
-    # Your code here
+#     # Your code here
 
+#     return merged_arr
 
-    return merged_arr
-
-# TO-DO: implement the Merge Sort function below recursively
-def merge_sort(arr):
-    # Your code here
+# # TO-DO: implement the Merge Sort function below recursively
 
 
-    return arr
+# def merge_sort(arr):
+#     # Your code here
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
-# utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
-# or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+#     return arr
+
+# # STRETCH: implement the recursive logic for merge sort in a way that doesn't
+# # utilize any extra memory
+# # In other words, your implementation should not allocate any additional lists
+# # or data structures; it can only re-use the memory it was given as input
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
+
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
+
+
+# def double(n):
+#     print(n)
+
+#     if n == 0:
+#         return
+
+#     double(n-1)
+#     double(n-1)
+
+
+# double(3)
+
+# def countdown(n):
+#     print(n)
+
+#     if n > 0:
+#         countdown(n-1)
+
+
+# countdown(4)
+
+# def fib(n):
+#     if n == 0:
+#         return 0
+#     if n == 1:
+#         return 1
+#     return fib(n - 1) + fib(n - 2)
+
+
+# fib(4)

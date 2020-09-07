fe70263c7dee2d19242949f3d42b1c56c412b398
Michelle Scott <scottmm374@gmail.com>
2020-09-07 12:57:20 -0400

    array sorting down to 1 element, and appending to new arr's

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,21 +1,38 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
-# def merge(arrA, arrB):
-#     elements = len(arrA) + len(arrB)
-#     merged_arr = [0] * elements
-
-#     # Your code here
-
-#     return merged_arr
-
-# # TO-DO: implement the Merge Sort function below recursively
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
 
 
-# def merge_sort(arr):
-#     # Your code here
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
+    merged_arr = [0] * elements
 
-#     return arr
+    return merged_arr
 
-# # STRETCH: implement the recursive logic for merge sort in a way that doesn't
+
+# TO-DO: implement the Merge Sort function below recursively
+
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        middle = len(arr) // 2
+        left = arr[:middle]
+        right = arr[middle:]
+        copy_left = []
+        copy_right = []
+
+        merge_sort(left)
+        if len(left) == 1:
+            print("only one on left")
+            copy_left.append(left[0])
+
+        merge_sort(right)
+        if len(right) == 1:
+            print("only one on right")
+            copy_right.append(right[0])
+
+
+merge_sort(arr1)
+# ! STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # # utilize any extra memory
 # # In other words, your implementation should not allocate any additional lists
 # # or data structures; it can only re-use the memory it was given as input
@@ -27,35 +44,3 @@
 
 # def merge_sort_in_place(arr, l, r):
 #     # Your code here
-
-
-# def double(n):
-#     print(n)
-
-#     if n == 0:
-#         return
-
-#     double(n-1)
-#     double(n-1)
-
-
-# double(3)
-
-# def countdown(n):
-#     print(n)
-
-#     if n > 0:
-#         countdown(n-1)
-
-
-# countdown(4)
-
-# def fib(n):
-#     if n == 0:
-#         return 0
-#     if n == 1:
-#         return 1
-#     return fib(n - 1) + fib(n - 2)
-
-
-# fib(4)
